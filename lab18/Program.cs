e83908185da79442a38635b3b950bbfe88505ff4
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-29 11:26:54 -0600

    CS 1400 C#

---
+++ lab18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace lab18
+{
+    class Program
+    {
+        // State Constants, Snake eyes, boxcars etc
+        static void Main(string[] args)
+        {
+            //ask for user input in WriteLine
+            //Do loop {
+            //readline the user's input and put it in a variable.
+            //if else statement for if the user pressed yes {
+            //create random number generator.
+            //create two integers for the random number generator for the dice.
+            //if statement for snake eyes 
+            //write "You rolled snake eyes"
+            //else if statement for boxcars 
+            //write "You rolled BoxCars"
+            //else statement that WriteLines the result for each die.
+            //close if statement }
+            // else if answer was no {
+            // print goodbye message and quit. }
+            // else respond that the input was incorrect and to please enter y or n }
+            //close out do While Loop }  while (userResponse != "y" || userResponse != "n");
+        }
+    }
+}
